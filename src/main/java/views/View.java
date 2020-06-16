0b156dcb49bbedca2ede55d0b557aa7ec7cc6469
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-17 00:05:36 +0100

    Moved controls

--- src/main/java/views/View.java
+++ src/main/java/views/View.java
@@ -3,11 +3,36 @@ package views;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 /* Class for abstract JavaFX views */
 public abstract class View {
+
+    protected final Stage stage;
+    protected File imageIn = null;
+
+    public View(Stage stage) {
+        this.stage = stage;
+    }
+
+    /* Runs a file chooser to set input file */
+    protected void runFileInputDialog(TextField target) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Image File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("PNG Files", "*.png"));
+        File selectedFile = fileChooser.showOpenDialog(stage);
+
+        if (selectedFile != null) {
+            imageIn = selectedFile;
+            target.setText(imageIn.getName());
+        }
+    }
 
     /* Handles fatal exceptions within Application thread */
     protected void handleFatalException(Throwable e) {
@@ -20,10 +45,10 @@ public abstract class View {
     }
 
     /* Runs salvageable IO exception dialog */
-    protected void runIOExceptionAlert(IOException ioException) {
+    protected void runIOExceptionAlert(IOException ioException, String coding) {
         Alert ioExceptionAlert = new Alert(Alert.AlertType.ERROR);
-        ioExceptionAlert.setTitle("Failed to Encode");
-        ioExceptionAlert.setHeaderText("Encoding Failed");
+        ioExceptionAlert.setTitle("Failed to " + coding);
+        ioExceptionAlert.setHeaderText(coding + " Failed");
         ioExceptionAlert.setContentText(ioException.getMessage());
         ioExceptionAlert.showAndWait();
     }
