9b94a29009c66789563e4593c3e9f58fda9cc5ff
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-17 20:02:16 +0100

    Call CLI if arguments are provided via command line

--- src/main/java/org/imster/Main.java
+++ src/main/java/org/imster/Main.java
@@ -1,9 +1,34 @@
 package org.imster;
 
+import org.imster.cli.Interpreter;
+import org.imster.cryptography.CryptoException;
+import org.imster.views.CodingView;
 import org.imster.views.TopView;
 
+import java.io.IOException;
+
+/* Application entry point */
 public class Main {
     public static void main(String[] args) {
-        TopView.initialise(args);
+
+        for (String a : args)
+            System.out.println(a);
+
+        // If no arguments provided, default to GUI
+        if (args.length == 0)
+            TopView.initialise(args);
+        else {
+            // Pass arguments to CLI interpreter
+            try {
+                Interpreter interpreter = new Interpreter(args);
+                interpreter.parse();
+                interpreter.execute();
+            }  catch (IllegalArgumentException | IOException e) {
+                System.err.println("Error: " + e.getMessage());
+                System.exit(1);
+            } catch (CryptoException e) {
+                CodingView.handleFatalException(e);
+            }
+        }
     }
 }
