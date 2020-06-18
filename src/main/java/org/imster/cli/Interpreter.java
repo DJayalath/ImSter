cbf24fb182aaa5c1e40426d8e0d11ad0872fec1b
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-18 14:52:16 +0100

    Updated help message with structure more similar to man pages

--- src/main/java/org/imster/cli/Interpreter.java
+++ src/main/java/org/imster/cli/Interpreter.java
@@ -164,9 +164,9 @@ public class Interpreter {
     /* Help function to show user usage. Called on argument errors */
     public static void printUsage() {
         System.out.println("\n\033[0;1mUSAGE");
-        System.out.println("\n  ---------- Encoding ----------\033[0;0m\n");
+        System.out.println("\n    ENCODING\033[0;0m\n");
         System.out.println("        java -jar ImSter-xxx.jar encode -i input.png -o output.png -m \"message\" -p password");
-        System.out.println("\n  \033[0;1m---------- Decoding ----------\033[0;0m\n");
+        System.out.println("\n\033[0;1m    DECODING\033[0;0m\n");
         System.out.println("        java -jar ImSter-xxx.jar decode -i input.png -p password\n");
     }
 
