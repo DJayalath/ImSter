d2c9a96adb08325e0b1db2497bfac5bd20315db5
Dulhan Jayalath <dhj1g19@soton.ac.uk>
2020-06-20 16:21:31 +0100

    Added CLI filter support for JPEG inputs when encoding

--- src/main/java/org/imster/cli/Interpreter.java
+++ src/main/java/org/imster/cli/Interpreter.java
@@ -121,8 +121,10 @@ public class Interpreter {
                 throw new IOException("Specified input path/file doesn't exist");
             } else if (!inputFile.isFile()) {
                 throw new IOException("Specified input path/file isn't a file");
-            } else if (!inputFile.getName().endsWith(".png")) {
-                throw new IOException("Specified input path/file does not have .png extension");
+            } else if (mode == ENCODE_MODE && !inputFile.getName().matches(".*\\.(png|PNG|jpg|JPG|jpeg|JPEG)")) {
+                throw new IOException("Specified input path/file does not have a standard png or jpg extension");
+            } else if (mode == DECODE_MODE && !inputFile.getName().matches(".*\\.(png|PNG)")) {
+                throw new IOException("Specified input path/file does not have a standard png extension");
             } else if (!inputFile.canRead()) {
                 throw new IOException("No read permission for specified input path/file");
             }
@@ -134,6 +136,8 @@ public class Interpreter {
             outputFile = new File(output);
             if (!isValidPath(output)) {
                 throw new IOException("Specified output path is invalid");
+            } else if (!outputFile.getName().endsWith(".png")) {
+                throw new IOException("Specified output file must have .png extension");
             }
         } else if (mode == ENCODE_MODE) {
             throw new IllegalArgumentException("Output image path not set. Use -o to set it");
